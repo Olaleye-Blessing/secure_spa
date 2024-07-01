f3ce3a2564ad841de597e9748831fee2c04817d9
Olaleye Blessing <Olayinkablexxy@gmail.com>
2024-07-01 18:34:25 +0100

    Create route to get new tokens

--- backend/app.js
+++ backend/app.js
@@ -40,6 +40,54 @@ app.post('/login', (req, res) => {
 	});
 });
 
+app.post('/refresh-token', (req, res) => {
+	const currentRefreshToken = req.cookies.refreshTk;
+
+	if (!currentRefreshToken)
+		return res.status(400).json({
+			status: 'fail',
+			message: 'Provide refresh token',
+		});
+
+	try {
+		const decodedToken = jwt.verify(
+			currentRefreshToken,
+			'my-refresh-token-secured-secret'
+		);
+
+		const name = decodedToken.name;
+		const user = { name };
+
+		const accessToken = jwt.sign(user, 'my-auth-token-secured-secret', {
+			expiresIn: '5m',
+		});
+		const refreshToken = jwt.sign(user, 'my-refresh-token-secured-secret', {
+			expiresIn: '5d',
+		});
+
+		const fiveDays = 5 * 24 * 60 * 60 * 1000;
+
+		res.cookie('refreshTk', refreshToken, {
+			httpOnly: true,
+			secure: true,
+			expires: new Date(Date.now() + fiveDays),
+		});
+
+		res.status(200).json({
+			status: 'success',
+			data: {
+				accessToken,
+				user: { name },
+			},
+		});
+	} catch (error) {
+		res.status(400).json({
+			status: 'fail',
+			message: 'Invalid refresh token',
+		});
+	}
+});
+
 app.listen(3000, () => {
 	console.log(`Server listening at port 3000`);
 });
