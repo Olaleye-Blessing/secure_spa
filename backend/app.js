40247ba7123a4d1c7eff2c2e0bee5692318ccbc4
Olaleye Blessing <Olayinkablexxy@gmail.com>
2024-07-01 17:45:00 +0100

    Create login route to get tokens

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+import jwt from 'jsonwebtoken';
 
 const app = express();
 
@@ -10,6 +11,33 @@ app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
 
 app.get('/', (req, res) => {
 	res.status(200).json({ status: 'success' });
+});
+
+app.post('/login', (req, res) => {
+	const name = req.body.name;
+	const user = { name };
+	const accessToken = jwt.sign(user, 'my-auth-token-secured-secret', {
+		expiresIn: '5m',
+	});
+	const refreshToken = jwt.sign(user, 'my-refresh-token-secured-secret', {
+		expiresIn: '5d',
+	});
+
+	const fiveDays = 5 * 24 * 60 * 60 * 1000;
+
+	res.cookie('refreshTk', refreshToken, {
+		httpOnly: true,
+		secure: true,
+		expires: new Date(Date.now() + fiveDays),
+	});
+
+	res.status(200).json({
+		status: 'success',
+		data: {
+			accessToken,
+			user: { name },
+		},
+	});
 });
 
 app.listen(3000, () => {
