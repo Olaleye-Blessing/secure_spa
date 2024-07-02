ac4839ed22a02dfb943a8eea495a9c0df2c585fc
Olaleye Blessing <Olayinkablexxy@gmail.com>
2024-07-02 06:45:50 +0100

    Display access token in the navbar

--- frontend/src/components/nav.jsx
+++ frontend/src/components/nav.jsx
@@ -1,6 +1,9 @@
 import { Link } from 'react-router-dom';
+import { useAuth } from '../hooks/useAuth';
 
 export default function Navbar() {
+	const accessToken = useAuth((state) => state.accessToken);
+
 	return (
 		<nav className='border-b'>
 			<ul className='flex items-center justify-center space-x-8 px-4 py-2'>
@@ -11,6 +14,7 @@ export default function Navbar() {
 					<Link to='/login'>Login</Link>
 				</li>
 			</ul>
+			<p className='px-4 pb-2 break-words'>User Token: {accessToken}</p>
 		</nav>
 	);
 }
